293750ba8ae7a2074366d68d98a57bcd800477d9
Tyler Neely <t@jujit.su>
2015-07-03 23:36:58 -0700

    Distribute cache and allow peers to be dynamically reconfigured.

--- sitecache.go
+++ sitecache.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"bytes"
+  "flag"
+  "strings"
+  "strconv"
 	"errors"
 	"io/ioutil"
 	"net/http"
@@ -43,13 +46,18 @@ func (w ProxyResponse) Write(body []byte) (int, error) {
 func (w ProxyResponse) WriteHeader(header int) {}
 
 func main() {
-	me := "http://10.0.0.1:9090"
+  rpcPort := flag.Int("rpcPort", 9090, "port for groupcache ipc")
+  proxyPort := flag.Int("proxyPort", 8080, "port for http proxy")
+  peerPort := flag.Int("peerPort", 7070, "port for receiving peer updates")
+
+  flag.Parse()
+
+	me := "http://localhost:" + strconv.Itoa(*rpcPort)
 	poolOpts := &groupcache.HTTPPoolOptions{
 		BasePath: "/stuff",
 	}
-	peers := groupcache.NewHTTPPoolOpts(me, poolOpts)
 
-	peers.Set(me)
+	cache := groupcache.NewHTTPPoolOpts(me, poolOpts)
 
 	groupcache.NewGroup("", 64<<20, groupcache.GetterFunc(
 		func(ctx groupcache.Context, key string, dest groupcache.Sink) error {
@@ -76,17 +84,19 @@ func main() {
 		},
 	))
 
+	cache.Set("http://localhost:9090", "http://localhost:9091", "http://localhost:9092")
+
 	proxy := goproxy.NewProxyHttpServer()
 	//proxy.Verbose = true
 	proxy.OnRequest().DoFunc(
 		func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
-			log.V(2).Info("Proxy got request for: %+v \n", *r.URL)
+		log.V(2).Infof("Proxy got request for: %+v", *r.URL)
 			r.URL = &url.URL{
 				Path: "/stuff/" + r.URL.Scheme + "://" + r.URL.Host + r.URL.Path,
 			}
 			pr := NewProxyResponse()
-			peers.ServeHTTP(pr, r)
-			log.V(2).Infof("body: %v", pr.response.Body)
+			cache.ServeHTTP(pr, r)
+			log.V(3).Infof("body: %v", pr.response.Body)
 			data, err := ioutil.ReadAll(pr.response.Body)
 			if err != nil {
 				log.V(1).Infof("Failed to read response %v", err)
@@ -95,5 +105,20 @@ func main() {
 		},
 	)
 
-	log.Fatal(http.ListenAndServe(":8080", proxy))
+	mux := http.NewServeMux()
+  mux.HandleFunc("/peers", func(w http.ResponseWriter, r *http.Request) {
+    data, err := ioutil.ReadAll(r.Body)
+    if err != nil {
+      log.V(1).Infof("Failed to read response %v", err)
+    }
+    cache.Set(strings.Split(string(data), ",")...)
+  })
+
+  log.Infoln("Starting rpc instance.")
+	go func() { log.Fatal(http.ListenAndServe(":" + strconv.Itoa(*rpcPort), cache)) }()
+
+  log.Infoln("Starting peer update listener.")
+  go func() { log.Fatal(http.ListenAndServe(":" + strconv.Itoa(*peerPort), mux)) }()
+  log.Infoln("Starting proxy instance.")
+	log.Fatal(http.ListenAndServe(":" + strconv.Itoa(*proxyPort), proxy))
 }
